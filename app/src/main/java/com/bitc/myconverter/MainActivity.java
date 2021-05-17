b13507845ca33a2e580c2d3dbcbd73c414a6bfab
JOKASTA <sijanneupane12@gmail.com>
2021-05-17 13:11:01 +0545

    converter created

--- app/src/main/java/com/bitc/myconverter/MainActivity.java
+++ app/src/main/java/com/bitc/myconverter/MainActivity.java
@@ -24,6 +24,32 @@ public class MainActivity extends AppCompatActivity {
         button= findViewById(R.id.button);
 
 
+        // on clicking button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //things to do after clicking button
+
+                // get temperature from the inputFaren in farenheit
+                String faren= inputFaren.getText().toString();
+
+                double fahrenheit = Double.parseDouble(faren);
+
+                //convert the temperature to celsius
+                // (32°F − 32) × 5/9 = 0°C
+
+                double celsius= ((5*(fahrenheit - 32.0))/9.0);
+
+                // display the output to the output
+
+                output.setText(" The temperature in celsius is= " + celsius);
+
+
+            }
+        });
+
+
 
 
 
